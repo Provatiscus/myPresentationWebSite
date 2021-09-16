e2ee2e29a29499d5d552aa3778a8c44e194d4e66
Provatiscus <pezeuy@gmail.com>
2021-09-16 21:55:42 +0200

    text-align:justify; added links to linkedin and github everywhere needed

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -20,18 +20,11 @@ urlpatterns = [
     # path for logout
 
     path(route='general', view=views.general, name='general'),
-    path(route='about', view=views.about, name='about'),
     path(route='contact', view=views.contact, name='contact'),
-    path(route='test', view=views.test, name='contact'),
     path(route='login', view=views.login_request, name='login_request'),
     path(route='logout', view=views.logout_request, name='login_request'),
     path(route='signup', view=views.registration_request, name='registration_request'),
     path(route='', view=views.general, name='index'),
-    path(route='by_id', view=views.get_dealerships_by_id, name='index'),
-    path(route='by_state', view=views.get_dealerships_by_state, name='get_dealerships_by_state'),
-    path(route='dealerships', view=views.dealerships, name='dealerships'),
-    path(route='dealer_details', view=views.get_dealer_details, name='reviews_by_id'),
-    path(route='post_review', view=views.add_review, name='post_review'),
     path(route='resumee', view=views.resumee, name='resumee'),
     path(route='gallery', view=views.gallery, name='resumee'),
     path(route='certificates', view=views.certificates, name='resumee'),
